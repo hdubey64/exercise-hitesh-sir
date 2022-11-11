df3ab9d694dabcd24f05840474db354c773f6115
hdubey64 <hdubey64@gmail.com>
2022-11-11 13:09:01 +0530

    About Section Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
+import AboutSection from "./AboutSection";
+import ContactSection from "./ConstactSection";
 import Header from "./Header";
 import NavBar from "./NavBar";
+import PortfolioSection from "./PortfolioSection";
 
 const App = () => {
    return (
@@ -8,6 +11,9 @@ const App = () => {
          <h1 style={{ textAlign: "center " }}>Exercise-0.2 by Hitesh Sir</h1>
          <NavBar />
          <Header />
+         <PortfolioSection />
+         <AboutSection />
+         <ContactSection />
       </div>
    );
 };
